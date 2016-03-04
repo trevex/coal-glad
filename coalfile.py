538079cf98f026c14ac578058478e717be7d9296
Nik Voss <niklas.voss@gmail.com>
2016-03-04 13:03:30 +0100

    Fixx...

--- coalfile.py
+++ coalfile.py
@@ -5,13 +5,13 @@ from os import path
 class GLADFile(CoalFile):
     url = "https://github.com/Dav1dde/glad.git"
     exports = ["include", "libs"]
-    options = {
+    settings = {
         "profile": "core" # default is core, see glad repository for more valid options
     }
     def prepare(self):
         git_clone(self.url, 'master', 'src')
     def build(self):
-        default_cmake_build('src/', 'build/', '-DGLAD_PROFILE="%s"' % self.options["profile"])
+        default_cmake_build('src/', 'build/', '-DGLAD_PROFILE="%s"' % self.settings["profile"])
     def package(self):
         cp('build/include', 'include')
         cp('build/*.a', 'libs/')
